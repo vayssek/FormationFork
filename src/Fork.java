a16c150bb505a27c616c8bcf0302fe462e84e1e4
Kevin Vaysse <vayssek@gmail.com>
2016-05-06 11:39:55 +0200

    ADDING METHOD COMMIT
    ADD : Completion of the method SendIt

--- src/Fork.java
+++ src/Fork.java
@@ -8,17 +8,32 @@ public class Fork {
 		// Read Keyboarb
 		Scanner readKeyb = new Scanner(System.in);
 		System.out.println("Enter an integer number : ");
-		int nb = readKeyb.nextInt();
-
+		int nbuser = readKeyb.nextInt();
 	}
 
-	public static SendInt(){
+	public static String SendInt(int nbuser){
 		int nb1 = 0;
 		int nb2 = 0;
 		int nb3 = 0;
 		for (int i = 0; i>=0; i++) {
-			
+			if(nbuser==-1){
+				break;
+			}else{
+				if(nbuser>nb1){
+					nb3=nb2;
+					nb2=nb1;
+					nb1=nbuser;}
+				if (nbuser>nb2 && nbuser<nb1){
+					nb3=nb2;
+					nb2=nbuser;
+				}
+				if(nbuser>nb3 && nbuser<nb2){
+					nb3=nbuser;
+				}
+				String str = nb1 + ", "+ nb2 + " and " + nb3;
+				return str;
+			}
 		}
+		return "end";
 	}
-
 }
