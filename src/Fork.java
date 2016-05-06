56e735393557e7fec0789250c52353687bf6681b
Fadul <fadul.fatouma@gmail.com>
2016-05-06 10:39:59 +0200

    ADD IN COMMIT
    Begin the method SendInt

--- src/Fork.java
+++ src/Fork.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Fork {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Read Keyboarb
+		Scanner readKeyb = new Scanner(System.in);
+		System.out.println("Enter an integer number : ");
+		int nb = readKeyb.nextInt();
+
+	}
+
+	public static SendInt(){
+		int nb1 = 0;
+		int nb2 = 0;
+		int nb3 = 0;
+		for (int i = 0; i>=0; i++) {
+			
+		}
 	}
 
 }
